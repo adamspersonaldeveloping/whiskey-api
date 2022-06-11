c4780a00062a591896daa92ac8ee176882981b59
John Adams <adamspersonaldeveloping@gmail.com>
2022-06-11 17:26:13 +0800

    hid datastring in env

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {MongoClient} = require('mongodb');
 const mongoClient = require('mongodb').MongoClient
-const app = express();
-const connectionString = 'mongodb+srv://adamspersonaldeveloping:PDs2KTOUPh3VMDHU@whiskey.rndpglw.mongodb.net/?retryWrites=true&w=majority'
+const app = express()
+
 const PORT = 42069
+require('dotenv').config()
 
+let db,
+dbConnectionStr = process.env.DB_STRING,
+dbName = 'whiskey'
 
-MongoClient.connect(connectionString, {
+MongoClient.connect(dbConnectionStr, {
     useUnifiedTopology: true 
 })
     .then(client => {
-        console.log('connected to databse "Whiskey"')
+        console.log(`connected to database ${dbName} Database`)
 
         const db = client.db('whiskey-api')
         const whiskeyCollection = db.collection('whiskeys')
@@ -23,7 +27,7 @@ MongoClient.connect(connectionString, {
         app.use(express.static('public'))
 
         app.get('/', (req, res) => {
-            whiskeyCollection.find().toArray()
+            whiskeyCollection.find().sort({maker: 1}).toArray()
                 .then(results => {
                     res.render('index.ejs', { whiskeys: results})
                 })
